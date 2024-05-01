0b0583800c478ee5700e04aa7bfe6138ded4a652
lancerzhang <lancerz@hotmail.com>
2024-05-01 23:00:45 +0800

    place go2 to usd

--- main.py
+++ main.py
@@ -1,20 +1,4 @@
 import omni
-from pxr import UsdGeom, Gf
-import carb
-
-def create_cube_and_camera(stage):
-    # Create a new Cube Primitive at the origin
-    cube = UsdGeom.Cube.Define(stage, '/World/Cube')
-    cube.GetPrim().GetAttribute("size").Set(2.0)
-
-    # Create a camera
-    camera = UsdGeom.Camera.Define(stage, '/World/Camera')
-    camera.GetPrim().GetAttribute("focalLength").Set(50)
-    camera.GetPrim().GetAttribute("horizontalAperture").Set(20.4)
-    camera.GetPrim().GetAttribute("verticalAperture").Set(15.3)
-
-    # Position the camera in front of the cube
-    camera.GetPrim().GetAttribute("xformOp:translate").Set(Gf.Vec3d(10, 0, 0))
 
 def get_camera_image():
     # Set up the viewport to use the camera
@@ -26,7 +10,7 @@ def get_camera_image():
     image = result.get()
     
     # Save the image to a file
-    image.save("/mnt/data/captured_image.png", "PNG")
+    image.save("captured_image.png", "PNG")
 
 def main():
     # Initialize application
@@ -35,9 +19,6 @@ def main():
     # Load a USD scene
     stage = omni.usd.get_context().get_stage()
     stage.Open("warehouse.usdc")
-
-    # Create cube and camera
-    create_cube_and_camera(stage)
 
     # Get an image from the camera
     get_camera_image()
